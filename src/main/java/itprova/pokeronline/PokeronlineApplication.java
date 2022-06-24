ec16bcb3c9db8e8646030125a986edac3add220c
eros fabbri <erosfabbridev@gmail.com>
2022-06-24 14:27:21 +0200

    primocommit

---
+++ src/main/java/itprova/pokeronline/PokeronlineApplication.java
@@ -0,0 +1,13 @@
+package itprova.pokeronline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeronlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeronlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/itprova/pokeronline/PokeronlineApplicationTests.java
@@ -0,0 +1,13 @@
+package itprova.pokeronline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeronlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
